37bd5f84277a942d59e1b68d4df546b6c9c5a4e8
Jon Sowman <jon@hexoc.com>
2012-05-24 15:25:00 +0100

    A bit of code refactoring to move the ISR stuff into logic

--- firmware/logic/logic.h
+++ firmware/logic/logic.h
@@ -8,11 +8,16 @@
 
 #include "system\typedefs.h"
 
+#define TIMER_PRELOAD			250
+
 void logicConfig(uint8_t options);
 void logicStart(void);
 void setSampleRate(uint16_t samplerate);
 uint16_t getSampleRate(void);
+void startTimer();
 
-void startTimer(uint16_t samplerate);
+// ISR prototypes
+void low_isr(void);
+void high_isr(void);
 
 #endif /* __LOGIC_H__ */
