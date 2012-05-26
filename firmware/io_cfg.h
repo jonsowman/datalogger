9c8c34f8eaa901222e37597355f74a9cef436c8a
David <cillian64@googlemail.com>
2012-05-26 17:30:55 +0000

    added pin #defines to io_cfg.h

--- firmware/io_cfg.h
+++ firmware/io_cfg.h
@@ -10,15 +10,8 @@
 
 /** U S B ***********************************************************/
 #define tris_usb_bus_sense  TRISAbits.TRISA1    // Input
-//mod by Tim
-//#if defined(USE_USB_BUS_SENSE_IO)
-//#define usb_bus_sense       PORTAbits.RA1
-//#else
-//remove "if" condition
 #define usb_bus_sense       1
-//#endif
 #define temp_sense			PORTDbits.RD7
-//end mod by Tim
 
 #define tris_self_power     TRISAbits.TRISA2    // Input
 
@@ -28,22 +21,14 @@
 #define self_power          1
 #endif
 
-//mod by Tim
 /** Switches ********************************************************/
 #define sw2                 PORTBbits.RB4
 
 /** P O T ***********************************************************/
 #define mInitPOT()          TRISAbits.TRISA0=1;ADCON0=0x01;ADCON2=0x3C;
 
-//end mod by Tim
-
 /** L E D ***********************************************************/
-//mod by Tim
-//Original
 #define mInitAllLEDs()      LATD &= 0xF0; TRISD &= 0xF0;
-//mod by Tim
-//#define mInitAllLEDs()      LATD &= 0xFF; TRISD &= 0x00;
-//end mod by Tim
 
 #define mLED_1              LATDbits.LATD0
 #define mLED_2              LATDbits.LATD1
@@ -64,5 +49,44 @@
 #define mLED_2_Toggle()     mLED_2 = !mLED_2;
 #define mLED_3_Toggle()     mLED_3 = !mLED_3;
 #define mLED_4_Toggle()     mLED_4 = !mLED_4;
+
+/***** Actual logic analyser pins ****************************/
+/******** SRAM Address Lines *******/
+#define A16		LATBbits.LATB2
+
+#define A15		LATDbits.LATD7
+#define A14		LATDbits.LATD6
+#define A13		LATDbits.LATD5
+#define A12		LATDbits.LATD4
+#define A11		LATDbits.LATD3
+#define A10		LATDbits.LATD2
+#define A9		LATDbits.LATD1
+#define A8		LATDbits.LATD0
+
+#define A7		LATBbits.LATB7
+#define A6		LATBbits.LATB6
+#define A5		LATBbits.LATB5
+#define A4		LATBbits.LATB4
+#define A3		LATBbits.LATB3
+
+#define A2		LATAbits.LATA5
+#define A1		LATAbits.LATA4
+#define A0		LATAbits.LATA3
+
+/********* SRAM Control Lines ********/
+#define CE		LATCbits.LATC1
+#define OE		LATCbits.LATC0
+#define	WE		LATCbits.LATC2
+#define	CE2		LATCbits.LATC6
+
+/****Shift Regiser Control Lines *****/
+#define SR_CLK		LATEbits.LATE0
+#define SR_CLK_EN	LATEbits.LATE1
+#define SR_PLOAD	LATEbits.LATE2
+#define SR_SEROUT	LATAbits.LATA2
+
+/**** Other *****/
+#define BUFFER_EN	LATCbits.LATC7
+
 
 #endif //IO_CFG_H
