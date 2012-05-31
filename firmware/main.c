f93fffeb0141ef74e402f2b6ba5f43db9e17a02a
Jon Sowman <jon@hexoc.com>
2012-05-31 11:22:58 +0100

    Checking each channel read

--- firmware/main.c
+++ firmware/main.c
@@ -58,18 +58,12 @@ void main(void)
 {        
     InitializeSystem();
     
-    LATLEDA = 1;
-    LATLEDB = 1;
-    
 	//pintest();
-	
-	//enableBuffer();
-	while(1);
 	
 	writeRAM(0); // write data to byte 0 on boot
 	while(1) {
 		uint8_t b = readRAM(0);
-		if(b & 0x02) LATLEDA = 1;
+		if(b & 0x01) LATLEDA = 1;
 		Delay10TCYx(60);
 	}
 		
